be878cb7d25316aff0a6dfac0aee03a08dba6786
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-10-22 12:08:58 +0200

    feat: form styling

--- login_page.php
+++ login_page.php
@@ -1,11 +1,18 @@
 <?php include 'layout.php'; ?>
 
+<div class="container mt-5">
     <!-- On action the file that handles the login -->
-    <form class="form-group" action="login.php" method="POST">
-        <label for="username">Username</label>
-        <input type="text" id="username" name="username">
-        <label for="password">Password</label>
-        <input type="text" id="password" name="password">
-        <button type="submit">Login</button>
+    <form action="login.php" method="POST">
+        <div class="mb-3">
+            <label for="username" class="form-label fw-bold">Username</label>
+            <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label fw-bold">Password</label>
+            <input type="password" id="password" name="password" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
     </form>
+</div>
 
