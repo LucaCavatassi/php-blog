730feb780cae538014921e1011d972fff300ee84
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-10-22 10:25:32 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <body>
+        <?php 
+
+        echo('Hello World');
+
+        
+
+
+
+
+        ?> 
+        <form action="GET">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username">
+            <label for="password">Password</label>
+            <input type="text" id="password" name="password">
+        </form>
+    </body>
+</html>
