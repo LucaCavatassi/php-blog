b0492542c47f1705cc4fe65ab912abdf87566e1e
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-10-22 17:00:01 +0200

    feat: create post form layout

---
+++ create_post_page.php
@@ -0,0 +1,8 @@
+<?php include('layout.php') ?>
+
+<div class="container mt-4">
+    <h1>Create new post</h1>
+    <form class="ms-form" action="" method="POST">
+        <h1>post</h1>
+    </form>
+</div>
