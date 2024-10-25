9da30b7f57c1fa3829e543b1fd8d5ad908e3889c
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-10-25 10:59:55 +0200

    feat: style of img

--- show_post_page.php
+++ show_post_page.php
@@ -10,7 +10,7 @@ if (isset($_GET['id'])) {
     $id = (int)$_GET['id'];
 
     // Prepare statement
-    $stmt = $mysqli->prepare("SELECT posts.id, posts.title, posts.content, posts.created_at, posts.updated_at, categories.name, users.username
+    $stmt = $mysqli->prepare("SELECT posts.id, posts.title, posts.content, posts.image, posts.created_at, posts.updated_at, categories.name, users.username
             FROM posts
             INNER JOIN categories ON posts.category_id = categories.id
             INNER JOIN users ON posts.user_id = users.id
@@ -36,12 +36,25 @@ if (isset($_GET['id'])) {
                 echo "<a href='home_page.php' class='btn btn-secondary'>&leftarrow; Show posts</a>";
             echo "</div>";
 
+            echo "<div>";
+            if (isset($post['image'])) {
+                echo '<img id="image-show" class="border border-primary rounded" src="' . htmlspecialchars($post['image']) . '" class="img-fluid" alt="...">';
+            }  else {
+                echo '<img id="image-show" src="https://media.istockphoto.com/id/1324356458/vector/picture-icon-photo-frame-symbol-landscape-sign-photograph-gallery-logo-web-interface-and.jpg?s=612x612&w=0&k=20&c=ZmXO4mSgNDPzDRX-F8OKCfmMqqHpqMV6jiNi00Ye7rE=" class="card-img-top" alt="...">';
+            }
+            echo "</div>";
+
+            echo "<h4 class='my-2'>Article</h4>";
             echo "<p>" . (htmlspecialchars($post['content'])) . "</p>";
+
+            echo "<hr>";
 
             echo "<div class='d-flex justify-content-between align-items-center mt-4'>";
                 echo "<p class='text-secondary'>Written by <i><strong> " . htmlspecialchars($post['username']) . " </i></strong></p>";
                 echo "<p class='text-secondary'>Category <i><strong> " . htmlspecialchars($post['name']) . " </i></strong></p>";
             echo "</div>";
+
+            echo "<hr>";
 
             echo "<div>";
                 echo "<p class='text-secondary'>Created on <i><strong> " . date_format($dateCreate, 'm/d/Y') . " </i></strong></p>";
