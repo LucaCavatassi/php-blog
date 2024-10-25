885ba75bd07299dca6c8d6fb1dc0c3bc8d7437bb
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-10-25 11:58:47 +0200

    fix: missing image on filter

--- filter_page.php
+++ filter_page.php
@@ -4,7 +4,7 @@ include 'layout.php';
 // New object that creates a connection to the db
 $mysqli = new mysqli("localhost", "root", "root", "my_blog_db");
 
-$sql = "SELECT posts.id, posts.title, posts.content, posts.created_at, posts.updated_at, categories.name, users.username
+$sql = "SELECT posts.id, posts.title, posts.content, posts.image, posts.created_at, posts.updated_at, categories.name, users.username
         FROM posts
         INNER JOIN categories ON posts.category_id = categories.id
         INNER JOIN users ON posts.user_id = users.id
